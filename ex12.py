be250c920234db4e29a9c774541de9f7a3a346d7
Grant Hazelton <grant@thehazeltons.f2s.com>
2015-05-24 10:18:46 +0100

    I mean to update ex12.py and add an empty ex13.py...

--- ex12.py
+++ ex12.py
@@ -1,1 +1,13 @@
-# @todo
+# I jumped ahead when doing exercise 11!
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r old, %r tall and %r heavy" % (
+    age, height, weight)
+
+# Remember: %r is for raw representation - debugging; %s is for plain output.
+
+# In PowerShell, to get documentation about the input function:
+# > py -m pydoc raw_input
