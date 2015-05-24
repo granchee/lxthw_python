2a49bc9f8ad2d0357efe882fe308fc8ed635964d
Grant Hazelton <grant@thehazeltons.f2s.com>
2015-05-24 09:54:33 +0100

    Finished exercise 11

--- ex11.py
+++ ex11.py
@@ -1,1 +1,15 @@
-# @todo
+print "How old are you?",
+age = raw_input()
+# See https://docs.python.org/2/library/functions.html#raw_input
+
+print "How tall are you?",
+height = raw_input()
+
+# print "How much do you weigh?",
+weight = raw_input('How much do you weigh? ')
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
+
+# s = '6\'2"'
+# print "%r" % s
